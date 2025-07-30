16ef988537ff6671bd28f9c671084b75745f6ed6
Edward Hawkes <edward.hawkes@protonmail.com>
2025-07-30 02:52:33 +0200

    Refactor !volume

--- music_commands.go
+++ music_commands.go
@@ -63,15 +63,19 @@ func pauseSong(s *discordgo.Session, m *discordgo.MessageCreate) {
 func setVolume(s *discordgo.Session, m *discordgo.MessageCreate) {
 	args := strings.Fields(m.Content)
 	if len(args) < 2 {
-		s.ChannelMessageSend(m.ChannelID, "Usage: !volume <value between 0.0 and 2.0>")
+		s.ChannelMessageSend(m.ChannelID, "Usage: !volume <value between 0 and 200>")
 		return
 	}
 
 	newVolume, err := strconv.ParseFloat(args[1], 64)
-	if err != nil || newVolume < 0.0 || newVolume > 2.0 {
-		s.ChannelMessageSend(m.ChannelID, "Invalid volume value. Please specify a number between 0.0 and 2.0.")
+	if err != nil || newVolume < 0.0 || newVolume > 200.0 {
+		s.ChannelMessageSend(m.ChannelID, "Invalid volume value. Please specify a number between 0 and 200.")
 		return
 	}
+	var preservedVolume float64 = newVolume
+	// Normalize the volume to a range of 0.0 to 2.0
+	newVolume = newVolume / 100.0 // Convert percentage to a factor
+
 
 	volumeMutex.Lock()
 	if _, ok := volume[m.GuildID]; !ok {
@@ -80,7 +84,7 @@ func setVolume(s *discordgo.Session, m *discordgo.MessageCreate) {
 	volume[m.GuildID] = newVolume
 	volumeMutex.Unlock()
 
-	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Volume set to %.2f", newVolume))
+	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Volume set to %.2f%%", preservedVolume))
 }
 
 func skipSong(s *discordgo.Session, m *discordgo.MessageCreate) {
