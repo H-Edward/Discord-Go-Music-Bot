f0ff49d03faf53695da34157d0ea796b219b054d
Edward Hawkes <edward.hawkes@protonmail.com>
2025-08-25 00:14:10 +0200

    Fix queue command deadlock

--- internal/commands/showqueue.go
+++ internal/commands/showqueue.go
@@ -11,17 +11,17 @@ import (
 func ShowQueue(s *discordgo.Session, m *discordgo.MessageCreate) {
 	state.QueueMutex.Lock()
 	defer state.QueueMutex.Unlock()
+
 	if len(state.Queue[m.GuildID]) == 0 {
 		s.ChannelMessageSend(m.ChannelID, "Queue is empty.")
 		return
 	}
+
 	// Make a formatted list of songs, "[N] URL""
-	state.QueueMutex.Lock()
 	var formattedQueue []string
 	for i, song := range state.Queue[m.GuildID] {
 		formattedQueue = append(formattedQueue, fmt.Sprintf("[%d] %s", i+1, song))
 	}
-	state.QueueMutex.Unlock()
 
 	s.ChannelMessageSend(m.ChannelID, "Current queue:\n"+strings.Join(formattedQueue, "\n"))
 }
