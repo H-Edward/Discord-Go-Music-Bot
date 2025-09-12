843917c2a5ebe21afb6ef6ef406c424c3627f4d5
Edward Hawkes <edward.hawkes@protonmail.com>
2025-09-12 16:17:56 +0200

    refactor: handlers to use commandSelector

--- internal/handlers/messagecreate.go
+++ internal/handlers/messagecreate.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"discord-go-music-bot/internal/commands"
 	"discord-go-music-bot/internal/constants"
 	"discord-go-music-bot/internal/state"
 	"log"
@@ -20,38 +19,5 @@ func HandleMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	ctx := state.NewMessageContext(s, m)
 
-	switch ctx.CommandName {
-	case "ping":
-		commands.Pong(*ctx)
-	case "pong":
-		commands.Ping(*ctx)
-	case "play":
-		commands.AddSong(*ctx, false) // false as in not a search
-	case "search":
-		commands.AddSong(*ctx, true) // true as in search for a song
-	case "skip":
-		commands.SkipSong(*ctx)
-	case "queue":
-		commands.ShowQueue(*ctx)
-	case "stop":
-		commands.StopSong(*ctx)
-	case "pause", "resume":
-		commands.PauseSong(*ctx)
-	case "volume":
-		commands.SetVolume(*ctx)
-	case "currentvolume":
-		commands.CurrentVolume(*ctx)
-	case "nuke": // delete n messages
-		commands.NukeMessages(*ctx)
-	case "uptime":
-		commands.Uptime(*ctx)
-	case "version":
-		commands.Version(*ctx)
-	case "help":
-		commands.Help(*ctx)
-	case "oss":
-		commands.Oss(*ctx)
-	default:
-		commands.Unknown(*ctx)
-	}
+	commandSelector(ctx)
 }
